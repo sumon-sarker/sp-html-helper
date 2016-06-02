483331a095127783a80f74e22ca557e19c698d71
lit.sumon <lit.sumon@gmail.com>
2016-06-02 17:28:16 +0600

    Updated Plugin Function

--- dist/js/sp-html-helper-config.js
+++ dist/js/sp-html-helper-config.js
@@ -1,7 +1,22 @@
 var SP = new SpHtmlHelper({
 	screenSize : 750,
-	muHahah : 600,
-	abcd : {
-		Npm:10
-	}
+	domainName : 'www.sumonsarker.com'
+});
+
+SP.menu({
+	containerId 	: 'SpHtmlHelper',
+	containerClass 	: 'SpHtmlHelper',
+	menuPosition 	: 0,
+	menuItems 		: [
+		{
+			item 		: 'SpHtmlHelper1',
+			link 		: '#',
+			css_class 	: 'first'
+		},
+		{
+			item 		: 'SpHtmlHelper2',
+			link 		: '#',
+			css_class 	: 'last'
+		},
+	]
 });
--- dist/js/sp-html-helper.js
+++ dist/js/sp-html-helper.js
@@ -1,23 +1,38 @@
 (function(){
 	
 	this.SpHtmlHelper = function(options){	/*Constructor*/
-		this.displayLog("SpHtmlHelper is getting Started!","color:green;font-size:20px");
-		var defaultOptions = {
+		this.displayLog("SpHtmlHelper Constructor Started!","font-size:20px");
+		this.APP 			= null;
+		var defaultOptions 	= {
+			domainName : window.location.origin,
 			screenSize : 768,
 		};
-		this.options = this.getDefaultOptions(defaultOptions,options);
+		this.APP = this.getDefaultOptions(defaultOptions,options);
+		this.displayLog(this.APP);
 	};
 
 	SpHtmlHelper.prototype.menu = function(options) {
-		this.displayLog("SpHtmlHelper Menu Started!","color:blue;font-size:20px");
+		this.displayLog("SpHtmlHelper Menu Started!","font-size:20px");
 		var defaultOptions = {
 			containerId 	: 'SpHtmlHelper',
 			containerClass 	: 'SpHtmlHelper',
 			menuPosition 	: 0,	/*0,1,2,3,4,5,6,N*/
+			menuItems 		: [
+				{
+					item 		: 'SpHtmlHelper',
+					link 		: '#',
+					css_class 	: 'active'
+				}
+			]
 		};
+		options = this.getDefaultOptions(defaultOptions,options);
+		this.displayLog(options);
+
+
+		this.makeSpMenu(options);
 	};
 
-	SpHtmlHelper.prototype.displayLog = function(message,css='font-size:10px;color:#3cf'){
+	SpHtmlHelper.prototype.displayLog = function(message,css='font-size:10px;color:green'){
 		console.log("%c%o",css,message);
 	};
 
@@ -28,7 +43,11 @@
 				defaultOptions[property] = options[property];
 			}
 		}
-		this.displayLog(defaultOptions);
+		return defaultOptions;
 	};
+
+	SpHtmlHelper.prototype.makeSpMenu = function(options){
+		
+	}
 
 }());
